b70348f545d1810805ddb613bdb2d9d7dc5104f2
Antonytm <antontishchenko@gmail.com>
2020-11-28 13:37:47 +0200

    Appveyor build: fix package creation

--- build/build.ps1
+++ build/build.ps1
@@ -4,10 +4,6 @@ if ($version -eq $null) {
 }
 "Package version: " + $version
 
-cd "lighthouse tools"
-npm install --save-dev
-cd ..
-
 Remove-Item build\package -Recurse -ErrorAction Ignore
 Remove-Item build\artifacts -Recurse -ErrorAction Ignore
 New-Item -Name build\package -ItemType directory
@@ -22,5 +18,5 @@ Copy-Item .\App_Config\Include\TinyEditor.config .\build\package\App_Config\Incl
 Copy-Item .\App_Config\Include\TinyMce.FieldType.config .\build\package\App_Config\Include
 Copy-Item .\serialization\* .\build\package\Data -recurse
 
-$packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\sitecore.lighthouse." + $version + ".update -r"
+$packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\TinyMCERTE." + $version + ".update -r"
 iex $packageCmd
